a4bca616321cf3eb12379dcec09384402b9fea88
kiage <rovielaundry@gmail.com>
2023-02-20 21:02:24 -0800

    printing last digit of number using modulo

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - random number assignment to variable
+ * show if n(random variable) is
+ * greater than 5, o or less than 6 and not 0
+ *
+ * return : always 0 (success)
+ */
+int main(void)
+{
+	int n, m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX /2;
+	m = n % 10;
+	if (m > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	else if (m == 0)
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+
+	return (0);
+}

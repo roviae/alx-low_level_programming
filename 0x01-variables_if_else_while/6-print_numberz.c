c949b14f8cdfec555098178b27acb64d8c1c4a2f
kiage <rovielaundry@gmail.com>
2023-02-20 22:14:30 -0800

    single digits print use putchar  use also

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print single digit numbers of base 10
+ * only use putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n ='0'; n <= '9'; n++)
+		putchar(n);
+	putchar(10);
+
+	return (0);
+}

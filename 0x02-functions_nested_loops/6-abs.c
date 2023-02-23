b534983e6f4ed591b542831ca9d1a72be5d38198
kiage <rovielaundry@gmail.com>
2023-02-22 19:22:47 -0800

    print absolute values of integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - fxn that computes the absolute value of an integer
+ *@int: fxn arguement
+ * Return: 0 Always
+ */
+int _abs(int)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+
+		return (abs_val);
+	}
+
+	return (c);
+
+	return (0);
+}

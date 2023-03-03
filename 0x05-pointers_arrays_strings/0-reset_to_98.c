cdc22bebd49c0f159e904ccf775ba3092be62469
kiage <rovielaundry@gmail.com>
2023-03-03 10:09:25 +0300

    fxn that takes pointer to an int and updates value it points to

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * function that takes a pointer to an int as parameter
- * and updates the value it points to to 98
- *@*n - pointer
- * 
- * Return: 0 Always
+ * reset_to_98 - fxn that takes a pointer to an int as paramete
+ * and updates value it points to
+ * @n: fxn param also a pointer
+ *
+ * Return : 0 Always
  */
 void reset_to_98(int *n)
 {

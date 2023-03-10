e61671f76ae6399473b77190032af5d3137e8544
kiage <123022606+roviae@users.noreply.github.com>
2023-03-10 16:59:56 +0300

    pointers again

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - fxn that prints an integer
+ * @n:fxn param
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+	if( (n = '45') != NULL)
+		{_putchar(-n);}
+	else
+		{_putchar(n);}
+
+	_putchar(10);
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
 /**
  * cap_string - fxn that capitalizes all string words
  * @cap: fxn parameter
@@ -8,8 +12,37 @@
  */
 char *cap_string(char *cap)
 {
-	char *start = cap;
+	int i = 0;
 
-	while (*cap)
+	while (cap[i])
 	{
-		if (*cap >= 'A
+		if (i == 0 && (cap[i] >= 'a' && cap[i] <= 'z'))
+			cap[i] -= 32;
+
+		if (check_separators(cap[i]) && (cap[i + 1] >= 'a' && cap[i + 1] <= 'z'))
+			cap[i + 1] -= 32;
+		i++;
+	}
+
+	return (s);
+}
+/**
+ * check_separators - separators of words, space, tabulation and newline
+ * @c: input character
+ *
+ * Return: 1 if separator, 0 otherwise
+ */
+int check_separators(char c)
+{
+	int i = 0;
+
+	char separators[13] = { ' ', '\t', '\n', ',', ';', '.', '!',
+	'?', "", '(', ')', '{', '}' };
+
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
+}

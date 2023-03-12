82eb0ae8dba6f7c70f103de5b1c65064d116b297
kiage <123022606+roviae@users.noreply.github.com>
2023-03-12 13:47:51 +0300

    pointers, arrays & strings3

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_diagsums - fxn that prints sum of 2 diagonols of
+ * a square matrix
+ * @a: pointer to array
+ * @size: array size
+ *
+ * Return: sum of diagonol values
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum;
+
+	for (a[0][size - 1])
+	{
+		for (a[size - 1][0])
+
+	putchar(sum);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme file for project 0x07-pointers_arrays_strings3

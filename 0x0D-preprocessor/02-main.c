16eb04f8d113a54c3f72f347621aa1706dc15df8
kiage <123022606+roviae@users.noreply.github.com>
2023-03-20 16:00:52 +0300

    preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the name of the file it
+ * was compiled from
+ * followed by a new line
+ *
+ * Return: name of file
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}

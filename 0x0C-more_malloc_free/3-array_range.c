426f24b93438d8be6d2fd565db3465423ab21497
kiage <123022606+roviae@users.noreply.github.com>
2023-03-26 21:08:47 +0300

    hotfixes

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	for (len = 0; len < (max - min); len++);
+	for (len = 0; len < (max - min); len++)
 	arr = malloc(sizeof(int) * (len + 1));
 	if (arr == NULL)
 		return (NULL);

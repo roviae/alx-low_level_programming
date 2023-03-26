667dd16591cb819b17c5a2de14e7df392e56292e
kiage <123022606+roviae@users.noreply.github.com>
2023-03-26 12:10:44 +0300

    return natural square roots only

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,16 +2,31 @@
 #include <math.h>
 
 /**
- * _sqrt_recursion - fxn that returns the natural sqrt
- * of a number
- * @n: number whose sqrt is to be returned
+ * sqrt_check - checks for the square root of c
+ * @g: guess at sqrt
+ * @c: number to find sqrt of
  *
- * Return: int
+ * Return: -1 or sqrt of c
+ */
+int sqrt_check(int g, int c)
+{
+	if (g * g == c)
+		return (g);
+	if (g * g > c)
+		return (-1);
+	return (sqrt_check(g + 1, c));
+}
+
+/**
+ * _sqrt_recursion - fxn that returns the natural square root
+ * of a number
+ * @n: square
+ *
+ * Return: natural sqrt or -1
  */
 int _sqrt_recursion(int n)
 {
-	_putchar(sqrt(n));
-
-	if (sqrt(n) == NaN)
-		return (-1);
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
 }

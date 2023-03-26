dbbddfbeba38a15b2a505a15bfa3a053f7cd4a1c
kiage <123022606+roviae@users.noreply.github.com>
2023-03-26 20:31:10 +0300

    concat two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,32 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 
 /**
- * str_concat - fxn that concatenates two strings
- * @s1: dest string
- * @s2: source string
+ * str_concat - Concatenate (combine) two strings
+ * @s1: First string
+ * @s2: Second string
  *
- * Return: 0
+ * Return: Pointer to new space in memory, NULL if it fails
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
+	char *s3, *empt;
+	int i, len1, len2, j;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	empt = "";
+	if (s1 == NULL)
+		s1 = empt;
+	if (s2 == NULL)
+		s2 = empt;
+	i = len1 = len2 = 0;
+	while (s1[len1] != '\0')
 	{
-		++s1;
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	len2++;
+	s3 = malloc((len1 + len2) * sizeof(*s1));
+	if (s3 == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		s3[i] = s1[i];
 		i++;
 	}
-	for (j = 0; s2[j] != '\0'; j++)
+	j = 0;
+	while (j <= len2)
 	{
-		*s1 = *s2;
-		s1++;
-		s2++;
+		s3[i] = s2[j];
+		i++;
 		j++;
 	}
-	printf("%s\n", s1);
-
-	return (0);
+	return (s3);
 }

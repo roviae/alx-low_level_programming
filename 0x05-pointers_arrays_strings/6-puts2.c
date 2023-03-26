6c016feb37e37a1b351701026bfac90ba0b6e61f
kiage <123022606+roviae@users.noreply.github.com>
2023-03-26 17:39:40 +0300

    print every other char of string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,5 +10,15 @@
  */
 void puts2(char *str)
 {
-	puts(str);
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	len -= 1;
+
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }

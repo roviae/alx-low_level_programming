60639cfb4f4388a8a0d3fe4ab302668398023773
kiage <123022606+roviae@users.noreply.github.com>
2023-03-26 16:06:05 +0300

    fxn that adds two ints

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - function that adds two integers and returns the resul
- *@a - fxn arg
- *@b - fxn arg
+ * @a: fxn arg
+ * @b: fxn arg
  * Return: Always 0
  */
 int add(int a, int b)

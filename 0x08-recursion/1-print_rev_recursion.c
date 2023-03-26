1fd7637e0c0609c88d4cb7094dffd96fd7e4864b
kiage <123022606+roviae@users.noreply.github.com>
2023-03-26 10:46:15 +0300

    reverse string using recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,12 +9,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i, length;
+	if (*s == '\0')
+		return;
 
-	length = strlen(s);
-
-	for (i = length; s[i] != '\0'; i--)
-	{
-		_putchar(s[i]);
-	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
